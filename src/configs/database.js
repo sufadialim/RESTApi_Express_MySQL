cfce06ff4deb610398bf3d441168aa9edd55bd01
Sufadi Alim <129618274+sufadialim@users.noreply.github.com>
2024-12-04 10:41:00 +0700

    Add files via upload

---
+++ src/configs/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+    multipleStatements  : true,
+    host                : 'localhost',
+    user                : 'root',
+    password            : 'heniheni',
+    database            : 'db_latihan_nodejs'
+};
